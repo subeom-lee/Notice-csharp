5ecf3dba82999ba57b0f0eb2d509de7c3f87927b
Subeom <lsb31431543@gmail.com>
2023-01-16 13:53:58 +0900

    게시물 작성 및 수정 할 때 제목이나 내용이 빈칸일때 에러메시지 수정

--- Models/Post.cs
+++ Models/Post.cs
@@ -6,7 +6,9 @@ namespace Notice.Models
     {
         [Key]
         public int post_id { get; set; }
+        [Required(ErrorMessage ="제목은 필수 입력 항목입니다.")]
         public string title { get; set; }
+        [Required(ErrorMessage = "내용은 필수 입력 항목입니다.")]
         public string contents { get; set; }
         public DateTime CreatedDatetime { get; set; } = DateTime.Now;
         public DateTime UpdatedDatetime { get; set; } = DateTime.Now;
